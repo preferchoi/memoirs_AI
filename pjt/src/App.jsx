0cb73c37d51c6ebb96612d9deff632466672a4ff
prefer_choi <prefer_choi@naver.com>
2023-10-11 18:45:55 +0900

    feat: response data printing

--- pjt/src/App.jsx
+++ pjt/src/App.jsx
@@ -1,11 +1,31 @@
-// import { useState } from 'react'
-
+import { useState } from 'react'
+import { CallGPT } from './apis/gpt';
 
 function App() {
+  const [data, setData] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
+  const handleClickAPICall = async () => {
+    try {
+      setIsLoading(true)
+      const message = await CallGPT();
+      setData(message)
+    } catch (err) {
 
+    } finally {
+      setIsLoading(false)
+    }
+  };
   return (
-    <></>
+    <>
+      <button onClick={handleClickAPICall}>GPT_API_CALL</button>
+      <div>
+        data: {data}
+      </div>
+      <div>
+        isLoading: {isLoading ? "loading.." : "finish!"}
+      </div>
+    </>
   )
 }
 
